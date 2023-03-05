184842c1445b2d58738b33bd9a8dadfeb85547df
Peggy Kalliani <pegztales@gmail.com>
2023-03-05 15:53:31 +0200

    Start on backend, create User Schema

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+// create User Schema
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    picturePath: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+//   automatic dates for creation, update etc....
+  { timestamps: true }
+);
+
+// CREATE AND EXPORT USER MODEL
+const User = mongoose.model("User", UserSchema);
+export default User;
