7532083408f3e092a9de15884e19de0e743dc4c5
Peggy Kalliani <pegztales@gmail.com>
2023-03-08 20:45:01 +0200

    changed proxy url

--- server/index.js
+++ server/index.js
@@ -82,8 +82,15 @@ mongoose
   .catch((error) => console.log(`${error} did not connect`));
 
   // added this for deployment (catchcall)
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  });
+}
 
   app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`))
