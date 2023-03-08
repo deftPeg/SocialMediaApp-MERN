726e5c282e0410a4ed90849f05bba77045013095
Peggy Kalliani <pegztales@gmail.com>
2023-03-08 19:29:29 +0200

    adjusting server file

--- server/index.js
+++ server/index.js
@@ -34,9 +34,9 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "client", "build")));
+// app.use("/assets", express.static(path.join(__dirname, "public/assets", "client", "build")));
 
-// app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // define an object with the connection parameters
 // useful when connection to MongoDB Atlas server
